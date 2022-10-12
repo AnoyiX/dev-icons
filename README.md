50398b13450afc4ab24cff433f7280f7889cda90
Anoyi <545544032@qq.com>
2022-10-12 18:44:13 +0800

    🎉 Init Project

---
+++ README.md
@@ -0,0 +1,43 @@
+
+<h3 align="center">Icons for Developer, Powered by DETA ⚡</h3>
+
+<hr>
+
+# Features
+
+- [Features](#features)
+- [Customization](#customization)
+  - [icons](#icons)
+  - [cols](#cols)
+- [Icons List](#icons-list)
+
+# Customization
+
+## icons
+
+Merge multiple icons in one svg
+
+```md
+![](https://icons.anoyi.com/?icons=html,css,docker,kubernetes)
+```
+
+[![](https://icons.anoyi.com/?icons=html,css,docker,kubernetes)](https://anoyi.com/dev-icons)
+
+
+## cols
+
+`Optional`, default value is `16`, 
+
+Change the `&cols=3` to any number between 1 and 50.
+
+```md
+![](https://icons.anoyi.com/?cols=3&icons=html,css,docker,kubernetes,apollo,git)
+```
+
+[![](https://icons.anoyi.com/?cols=3&icons=html,css,docker,kubernetes,apollo,git)](https://anoyi.com/dev-icons)
+
+# Icons List
+
+Please visite [https://anoyi.com/dev-icons](https://anoyi.com/dev-icons) to see all icons with names.
+
+![](https://icons.anoyi.com/?cols=16)
---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+import math
+from pathlib import Path
+from fastapi import FastAPI, Response
+
+# default svg params
+ICONS_COLS = 16
+GAP = 48
+ICON_SIZE = 256 + GAP
+PADDING = 0
+
+# svg files
+FILES = os.listdir('./icons')
+ICON_NAMES = {file.replace('.svg', '') for file in FILES}
+ICONS = {file.replace('.svg', '').lower(): Path(f'./icons/{file}').read_text() for file in FILES}
+
+
+def generate_svg(icon_names, cols):
+    svgs = [ICONS[x.lower()] for x in icon_names]
+    width = min(cols * ICON_SIZE, len(icon_names) * ICON_SIZE) + PADDING - GAP
+    height = math.ceil(len(icon_names) / cols) * ICON_SIZE + PADDING - GAP
+    return f"""
+    <svg width="{width}" height="{height}" viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg" version="1.1">
+    {''.join([f'<g transform="translate({(index % cols) * ICON_SIZE + PADDING}, {math.floor(index / cols) * ICON_SIZE + PADDING})">{icon}</g>' for index, icon in enumerate(svgs)])}
+    </svg>
+    """.strip()
+
+app = FastAPI()
+
+@app.get("/")
+async def svg(icons: str = '', cols: int = 16):
+    """Merge icons to a svg
+
+    Args:
+        icons (str): icons name
+        theme (str, optional): icons theme. Defaults to 'dark'.
+        cols (int, optional): how many icons per line. Defaults to 16.
+
+    Returns:
+        _type_: svg file
+    """
+    icon_names = ICON_NAMES if not icons else icons.split(',')
+    icon_names = list(filter(lambda x: x in ICON_NAMES, icon_names))
+    if not icon_names:
+        return Response('', status_code=404)
+    svg = generate_svg(icon_names, cols)
+    return Response(svg, media_type='image/svg+xml')
+
+
+@app.get("/api/icons")
+async def all():
+    """List all icons
+
+    Returns:
+        _type_: icons dict
+    """
+    # files = [file for file in FILES]
+    # native_icons = [{'name': file.replace('.svg', ''), 'themed': False} for file in list(filter(lambda x: len(x.split('.')) == 2, files))]
+    # themed_icons_name = {file.split('.')[0] for file in list(filter(lambda x: len(x.split('.')) == 3, files))}
+    # themed_icons = [{'name': name, 'themed': True} for name in themed_icons_name]
+    return sorted(ICON_NAMES)
